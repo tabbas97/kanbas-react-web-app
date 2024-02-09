16aa5a572fa1e7f0750a6c664b426d3e4be589fc
Thameem Abbas <tabbas97@gmail.com>
2024-02-08 22:40:38 -0500

    Revert index.tsx to original state

--- src/index.tsx
+++ src/index.tsx
@@ -3,33 +3,33 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+// import { StrictMode } from "react";
+// import { createRoot } from "react-dom/client";
 
 const rootElement = document.getElementById("root");
 
-if (rootElement !== null) {
-  const root = createRoot(rootElement);
-  root.render(
-    <StrictMode>
-      <App />
-    </StrictMode>
-  );
-} else {
-  console.error("Could not find root element");
-  console.error(document);
-  console.error(rootElement);
-  throw new Error("Could not find root element");
-}
+// if (rootElement !== null) {
+//   const root = createRoot(rootElement);
+//   root.render(
+//     <StrictMode>
+//       <App />
+//     </StrictMode>
+//   );
+// } else {
+//   console.error("Could not find root element");
+//   console.error(document);
+//   console.error(rootElement);
+//   throw new Error("Could not find root element");
 // }
-// const root = ReactDOM.createRoot(
-//   document.getElementById('root') as HTMLElement
-// );
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
+// }
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
